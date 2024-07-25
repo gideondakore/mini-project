c50bc86d13866b252aeeb1272062a2fd97f98d53
gideondakore <armstrongspycon27@gmail.com>
2024-07-25 10:39:03 -0400

    Added Vercel Analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,34 +17,38 @@ import {
 } from "./pages";
 import MapContainer from "./pages/MapWorks/MapContainer";
 import LoginForm from "./pages/TestEmailPage";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register">
-        <Route index element={<Register />} />
-        <Route path=":id" element={<Register />} />
-      </Route>
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/user" element={<UserInfo />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register">
+          <Route index element={<Register />} />
+          <Route path=":id" element={<Register />} />
+        </Route>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/user" element={<UserInfo />} />
 
-      <Route path="/map">
-        <Route index element={<MapContainer />} />
-        <Route path=":id" />
-      </Route>
+        <Route path="/map">
+          <Route index element={<MapContainer />} />
+          <Route path=":id" />
+        </Route>
 
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contacts />} />
-      <Route path="/hostel-details" element={<HostelDetails />} />
-      <Route path="payment" element={<PayStack />} />
-      <Route path="chat" element={<Chat />} />
-      <Route path="email" element={<LoginForm />} />
-      <Route path="verify-email" element={<EmailVerify />} />
-      <Route path="verification-success" element={<VerificationSuccess />} />
-      <Route path="verification-error" element={<VerificationError />} />
-      <Route path="email-sent" element={<EmailSentPage />} />
-    </Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contacts />} />
+        <Route path="/hostel-details" element={<HostelDetails />} />
+        <Route path="payment" element={<PayStack />} />
+        <Route path="chat" element={<Chat />} />
+        <Route path="email" element={<LoginForm />} />
+        <Route path="verify-email" element={<EmailVerify />} />
+        <Route path="verification-success" element={<VerificationSuccess />} />
+        <Route path="verification-error" element={<VerificationError />} />
+        <Route path="email-sent" element={<EmailSentPage />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 };
 
